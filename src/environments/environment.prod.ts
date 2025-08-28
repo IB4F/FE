748b4f3a2b6767904732bb8ebeb86ded95ec1199
IB4F <fabiano.meoo98@gmail.com>
2025-08-28 09:13:50 +0200

    change the api url for production and removed the folder envirorment since wasnot used inside the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com',
+  apiUrl: 'https://braingainalbania.al',
   stripePublishableKey: 'pk_live_replace_me'
 };
 
